2327f6c86d5158178ed6192774b2c0965d51260c
davidhro <99734963+davidhro@users.noreply.github.com>
2022-12-01 11:49:09 +0100

    Update main.c

--- Project_1/src/main.c
+++ Project_1/src/main.c
@@ -313,17 +313,28 @@ ISR(ADC_vect)
     
     
 //Cursors
+int CursorX = 0;
+int CursorY = 0;
+
 if (channel == 0) //osa x
 {
 value = ADC;
 if (value > 600)
 {
 CursorX++;
+if (CursorX > 6)
+{
+CursorX = 0;
+}
 lcd_gotoxy(CursorX, CursorY);
 }
 else if (value < 400)
 {
 CursorX--;
+if (CursorX < 0)
+{
+CursorX = 6;
+}
 lcd_gotoxy(CursorX, CursorY);
 }
 else
